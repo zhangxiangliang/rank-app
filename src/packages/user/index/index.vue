d765e8ef6cceae8c7e98490a201eae26a6b5ca72
zhangxiangliang <admin@pushme.top>
2021-02-24 00:04:44 +0800

    feat: 增加联系作者和赞赏码

--- src/packages/user/index/index.vue
+++ src/packages/user/index/index.vue
@@ -4,15 +4,52 @@
       <view class="avatar">
         <open-data type="userAvatarUrl"></open-data>
       </view>
+
       <view class="text-lg">
         <open-data type="userNickName"></open-data>
-      </view>
-      <view class="margin-top-sm">
-        <text>陪伴您每一个夜晚</text>
       </view>
 
       <image mode="scaleToFill" class="gif-wave" src="@/static/avatar/wave.gif">
       </image>
+    </view>
+
+    <view class="cu-list menu margin-bottom-xl shadow-lg">
+      <view class="cu-item arrow" @click="onPriviewQrcode">
+        <view class="content">
+          <text class="cuIcon-appreciatefill text-red"></text>
+          <text class="text-grey">赞赏支持</text>
+        </view>
+      </view>
+
+      <view class="cu-item arrow">
+        <view class="content">
+          <text class="cuIcon-writefill text-cyan"></text>
+          <text class="text-grey">联系作者</text>
+          <button class="contact" open-type="contact"></button>
+        </view>
+      </view>
+
+      <!-- <view class="cu-item arrow">
+        <navigator
+          class="content"
+          url="/pages/about/log/log"
+          hover-class="none"
+        >
+          <text class="cuIcon-formfill text-green"></text>
+          <text class="text-grey">更新日志</text>
+        </navigator>
+      </view> -->
+
+      <!-- <view class="cu-item arrow">
+        <navigator
+          class="content"
+          url="/pages/about/about/about"
+          hover-class="none"
+        >
+          <text class="cuIcon-infofill text-orange"></text>
+          <text class="text-grey">关于我们</text>
+        </navigator>
+      </view> -->
     </view>
   </view>
 </template>
@@ -26,13 +63,27 @@ import { State, Action, Getter, Mutation } from "vuex-class";
 import { User } from "@/store/user";
 
 // 帮助函数
-import { CONFIG_APP_NAME } from "@/config";
+import { CONFIG_APP_NAME, CONFIG_QRCODE } from "@/config";
 
 @Component({})
 export default class UserIndex extends Vue {
+  public CONFIG_QRCODE = CONFIG_QRCODE;
   public CONFIG_APP_NAME = CONFIG_APP_NAME;
+
+  onPriviewQrcode() {
+    uni.previewImage({
+      urls: [CONFIG_QRCODE],
+    });
+  }
 }
 </script>
 
 <style lang="scss">
+.contact {
+  display: inline-block;
+  position: absolute;
+  width: 100%;
+  background: salmon;
+  opacity: 0;
+}
 </style>
